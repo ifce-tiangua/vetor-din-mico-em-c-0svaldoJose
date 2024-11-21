9bc386e33a5915f81fbfd1c084925021354caee2
Osvaldo <107486317+0svaldoJose@users.noreply.github.com>
2024-11-21 13:01:03 -0300

    02

--- main.c
+++ main.c
@@ -4,15 +4,22 @@
 int main() {
     int *v, tamanho, i;
     scanf("%d", &tamanho);
-    v = (int*)malloc(tamanho*sizeof(int));
-    if(v == NULL) {
-        printf("[VAZIO]");
+    v = (int*)malloc(tamanho * sizeof(int));
+    if(v == NULL|| tamanho == 0) {
+        printf("[vazio]");
     } else {
         for(i = 0; i < tamanho; i++) {
             scanf("%d", &v[i]);
         }
-        for(i = 0; i < tamanho; i++)
-            printf("%d\t", v[i]);
+        printf("[");
+        for(i = 0; i < tamanho; i++) {
+            printf("%d", v[i]);
+            if(i < tamanho - 1) {
+                printf(", ");
+            }
+        }
+        printf("]");
+        free(v);
     }
-    free(v);
+    return 0;
 }
